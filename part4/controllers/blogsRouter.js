4319f296cb1b48305aa877b5bf2a990b0dbac4f1
rikudi <rikukaartoaho@gmail.com>
2023-12-10 00:41:24 +0200

    4.1.-4.2.

---
+++ part4/controllers/blogsRouter.js
@@ -0,0 +1,63 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/mongo')
+
+//HTTP GET ALL REQUEST
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blog => {
+    response.json(blog)
+  })
+})
+//HTTP GET REQUEST BY ID
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+//HTTP POST REQUEST
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+//HTTP PUT REQUEST
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
